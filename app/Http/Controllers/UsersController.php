ef45e692cb48e1b166ac5c2a2b71171397728c7c
Jesray Garciano <jesray.garciano@gmail.com>
2018-09-14 17:25:11 +0800

    Favorites folder added to Navigation bar for Users who favorited a posts

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    //
+
+		/**
+		 * Get all favorite posts by user
+		 *
+		 * @return Response
+		 */
+		public function myFavorites()
+		{
+		    $myFavorites = Auth::user()->favorites;
+
+		    return view('users.my_favorites', compact('myFavorites'));
+		}
+
+}
--- app/Post.php
+++ app/Post.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App;
-
+use App\Favorite;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     //
+
+	/**
+	 * Determine whether a post has been marked as favorite by a user.
+	 *
+	 * @return boolean
+	 */
+	public function favorited()
+	{
+	    return (bool) Favorite::where('user_id', Auth::id())
+	                        ->where('post_id', $this->id)
+	                        ->first();
+	}    
 }
