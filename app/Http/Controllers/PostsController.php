ed8fd9a676d7fa4d5951b4801c457b130f27fe63
Jesray Garciano <jesray.garciano@gmail.com>
2018-09-14 16:49:11 +0800

    Added controllers posts and users

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    //
+    /**
+	*Display a paginated list of posts.
+	*
+	* @return Response
+    */
+
+    public function index()
+    {
+    	$posts = Post::paginate(5);
+
+    	return view('posts.index', compact('posts'));
+    }
+}
